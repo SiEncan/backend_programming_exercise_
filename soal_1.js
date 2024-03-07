c90eedd68bba2922a289910433c93ff010f789be
SiEncan <adnanalvitomustaqim@gmail.com>
2024-03-07 16:25:23 +0700

    535230216_AdnanAlvitoMustaqim

--- soal_1.js
+++ soal_1.js
@@ -1,7 +1,7 @@
 for (let number = 2; number < 1000; number++) {
     let isPrime = true;
     for(let i = 2; i < number; i++) {
-        if(number % i === 0) {
+        if(number % i == 0) {
             isPrime = false;
             break;
         }
